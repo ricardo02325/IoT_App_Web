13a06f7f8abd0a44fd8f24f505fdd75a1fcb9d3f
ricardo02325 <rgregorio0.@ucol.mx>
2025-10-12 18:18:51 -0600

    l

--- resources/views/salones/salones.blade.php
+++ resources/views/salones/salones.blade.php
@@ -12,12 +12,7 @@
 
 @section('content')
     <main class="body-seccion13">
-    <div class="mapa-container"> 
         <img id="diagrama13" src="{{ asset('imgs/FIE.png') }}" alt="Mapa de la Facultad de Ingeniería Eléctrica">
-
-        <div class="image-credit">
-            Crédito del mapa: <a href="https://codepen.io/ramonfie-the-sasster/pen/bGyWzrO" target="_blank">[JIMENEZ BETANCOURT RAMON OCTAVIO]</a>
-        </div>
 
         <!-- Marcadores interactivos -->
         <!-- <div class="circle13">
