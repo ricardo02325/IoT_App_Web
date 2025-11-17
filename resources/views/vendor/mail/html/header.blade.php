876cf63c6682e1d5282f85a6bb3eeef5c4911ace
Cristian <cmelendez0@ucol.mx>
2025-11-17 13:30:41 -0600

    Version recuperar contraseña funcional - nueva version

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,10 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+
+   <img src="https://i.imgur.com/lf84VtB.png" class="logo" alt="Senstemp Logo" style="width: 100px; height: auto; margin: 0 auto; display: block;">
+
+</a>
+</td>
+</tr>
